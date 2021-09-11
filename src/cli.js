975320c60e0f672839363a813f53f0cf31a16080
Mister Fox <iscraw@yandex.ru>
2021-09-11 20:17:57 +0300

    linter

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const whatName = () => {
-    let name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-}
+const whatName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
+export default whatName;
